eaa23a7a679a4831448a53becea3d616f29a1ef0
joe-amos-waneno <waneno.amos@gmail.com>
2023-09-13 10:02:12 -0700

    handle path

---
+++ path_hundler.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * path_separator - separate the path values
+ * @arg: accept command
+ * 
+ *
+ * Return: string
+ */
+
+char *path_separator(char *path, char *arg)
+{
+	char *copy_path, *path_token, *file_path;
+	char *delim = ":";
+
+	/*path = return_path();*/
+
+	copy_path = strdup(path);
+	path_token = strtok(copy_path, delim);
+	file_path = malloc(strlen(arg) + strlen(path_token) + 2);
+
+	while(path_token != NULL)
+	{
+		strcpy(file_path, path_token);
+		strcat(file_path, "/");
+		strcat(file_path, arg);
+		strcat(file_path, "\0");
+
+		if (access(file_path, X_OK) == 0)
+		{
+			/*free(copy_path);*/
+			return (file_path);
+		}
+		path_token = strtok(NULL, delim);
+	}
+	free(copy_path);
+	free(file_path);
+	return (NULL);
+}
+
+char *return_path(void)
+{
+	char *path = NULL;
+
+	path = getenv("PATH");
+
+	if (path)
+	{
+		/*path = path_location(path);*/
+		return (path);
+	}
+	return (NULL);
+}
+
