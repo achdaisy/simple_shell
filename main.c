9301c03c9a366e48736db4b6d8162d43b6a52e73
achdaisy <oduordaizy@gmail.com>
2023-09-14 11:30:01 -0700

    spoilt main file

--- main.c
+++ main.c
@@ -8,10 +8,8 @@
 
 int main(__attribute__((unused)) int argc, char **argv, char **penviron)
 {
-	char *input = NULL, **cmd_args = NULL, buffer;
-	/*char *filepath = NULL*/
+	char *input = NULL, *arg1, **cmd_args = NULL, /*buffer*/ *path_var, *filepath;
 	size_t n = 0;
-	/*int i, terminal = isatty(0);*/
 	int incrementor = 0;
 	ssize_t chars_read = 0;
 	(void) argv;
@@ -20,37 +18,46 @@ int main(__attribute__((unused)) int argc, char **argv, char **penviron)
 	{
 		incrementor++;
 		if (isatty(STDIN_FILENO))/*interactive mode*/
-			/*console_display();*/
 			printf("shell$ ");
 		chars_read = getline(&input, &n, stdin);
-		if (chars_read == -1)/*handles EOF*/
-		{
-			free_arr(cmd_args);
-			free(input);
-                       /* exit(0);*/
-			return (-1);
-		}
+		if (chars_read == -1)
+                {
+                        free(input);
+                        /*free_arr(cmd_args);*/
+                        exit (0);
+                }
 		cmd_args = parser(input);
-		
 		if (cmd_args[0] == NULL)
 		{
+		 	free_arr(cmd_args);
+			continue;
+		}
+		/*if (chars_read != -1 && cmd_args[i] != NULL)*/
+		if (strcmp(cmd_args[0], "exit") == 0)
+		{
 			free(cmd_args);
-			continue;
+			free(input);
+			exit(0);
 		}
-		if (access(cmd_args[0], X_OK) == -1)
+		if (strcmp(cmd_args[0], "env") == 0)
 		{
-			buffer = (incrementor + '0');
-			input_err(argv[0], buffer, cmd_args[0]);
-			continue;
+			_environ();
 		}
-		/*if (access(cmd_args[0], X_OK) == -1)
-		{
-			printf("%s: %s No such file or directory\n", argv[0], cmd_args[0]);
-		}*/
-		exec(cmd_args, penviron);
-		free_arr(cmd_args);
+		path_var = return_path();
+		arg1 = cmd_args[0];
+		filepath = path_separator(path_var, arg1);
+		/*if (access(filepath, X_OK) == 0)
+                {
+                }
+                else
+                {
+                        buffer = (incrementor + '0');
+                        input_err(argv[0], buffer, cmd_args[0]);
+                }*/
+		exec(filepath, cmd_args, penviron);
+
 	}
 	free(input);
-	/*free_arr(cmd_args);*/
+	free_arr(cmd_args);
 	return (0);
 }
