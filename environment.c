a8f719006a128fa06aac8f3a3d7f4ff366f443b5
achdaisy <oduordaizy@gmail.com>
2023-09-14 11:31:38 -0700

    custom env function

---
+++ environment.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _environ - prints the current environment
+ *
+ * Return: nothing
+ */
+
+void _environ(void)
+{
+	/*extern char environ is defined in main.h*/
+	int pos = 0;
+	char **env = environ;
+	
+	for(pos = 0; env[pos] != NULL; pos++)
+	{
+		print_a_str(env[pos]);
+	}
+}
