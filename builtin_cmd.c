0d2b74049549e157d7a3e177fb8a498bf148609a
achdaisy <oduordaizy@gmail.com>
2023-09-20 01:26:22 -0700

    used custom _strcmp

--- builtin_cmd.c
+++ builtin_cmd.c
@@ -10,13 +10,13 @@
 
 void built_in(char **cmd_args, char *input)
 {
-	if (strcmp(cmd_args[0], "exit") == 0)
+	if (_strcmp(cmd_args[0], "exit") == 0)
 	{
 		free(cmd_args);
 		free(input);
 		exit(0);
 	}
-	if (strcmp(cmd_args[0], "env") == 0)
+	if (_strcmp(cmd_args[0], "env") == 0)
 	{
 		_environ();
 	}
