972cc51ba055177711d6fab8a9cd41c0c4cb2e71
achdaisy <oduordaizy@gmail.com>
2023-09-20 01:23:21 -0700

    added a custom getenv function

--- environment.c
+++ environment.c
@@ -9,11 +9,36 @@
 void _environ(void)
 {
 	/*extern char environ is defined in main.h*/
-	int pos = 0;
+	int pos;
 	char **env = environ;
 
 	for (pos = 0; env[pos] != NULL; pos++)
 	{
 		print_a_str(env[pos]);
 	}
+}
+
+/**
+ * _getenv - custom function to fetch environment variables
+ * @str: the string to be fetched from the environment
+ *
+ * Return: the specified environment var, else null
+ */
+
+char *_getenv(char *str)
+{
+	char **env = environ;
+	size_t len;
+	int pos, value;
+
+	len = _strlen(str);
+	for (pos = 0; env[pos] != NULL; pos++)
+	{
+		value = strncmp(str, env[pos], len);
+		if (value == 0 && env[pos][len] == '=')
+		{
+			return (&env[pos][len + 1]);
+		}
+	}
+	return (NULL);
 }
