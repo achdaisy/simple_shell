24c6ec34221bfbfb0f79e83a2f76a8e14c428e06
achdaisy <oduordaizy@gmail.com>
2023-09-20 19:04:45 -0700

    restoring the exit status

--- builtin_cmd.c
+++ builtin_cmd.c
@@ -16,17 +16,8 @@ void built_in(char **cmd_args, char *input)
 	{
 		free(cmd_args);
 		free(input);
-		exit(2);
+		exit(0);
 	}
-	/*if ((_strcmp(cmd_args[0], "exit") == 0) && (cmd_args[1] != NULL))*/
-	/**
-	*{
-		*stat = atoi(cmd_args[1]);
-		*free(cmd_args);
-		*free(input);
-		*exit(stat);
-	*}
-	*/
 	if (_strcmp(cmd_args[0], "env") == 0)
 	{
 		_environ();
